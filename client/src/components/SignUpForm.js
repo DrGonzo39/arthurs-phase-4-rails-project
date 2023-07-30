6058524eb0ab77414543805d2ffac83d49dc5e2d
Arthur Benware <artpolitic91@gmail.com>
2023-07-30 18:54:37 -0400

    organized a bit and created SignUpForm component

---
+++ client/src/components/SignUpForm.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { useState } from 'react';
+
+function SignUpForm({ onLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [errors, setErrors] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        password_confirmation: passwordConfirmation,
+      }),
+    })
+      .then((r) => {
+        if (r.ok) {
+            r.json().then((user) => onLogin(user))
+        } else {
+            r.json().then((error) => setErrors(error.errors))
+        }
+      });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="username">Username:</label>
+      <input
+        type="text"
+        id="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <label htmlFor="password">Password:</label>
+      <input
+        type="password"
+        id="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <label htmlFor="password_confirmation">Confirm Password:</label>
+      <input
+        type="password"
+        id="password_confirmation"
+        value={passwordConfirmation}
+        onChange={(e) => setPasswordConfirmation(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+    );
+}
+
+
+export default SignUpForm;
