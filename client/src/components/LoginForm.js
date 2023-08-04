9541a96af7b7682bf995ecb1c0cb93257cb9c5c9
Arthur Benware <artpolitic91@gmail.com>
2023-08-03 20:44:33 -0400

    completed LoginForm

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { UserContext } from "../context/user";
+
 
 function LoginForm({ onLogin }) {
     const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("")
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState([]);
+    const { onLogin } = useContext(UserContext);
   
     function handleSubmit(e) {
       e.preventDefault();
@@ -12,10 +16,14 @@ function LoginForm({ onLogin }) {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ username }),
-      })
-        .then((r) => r.json())
-        .then((user) => onLogin(user));
-    }
+      }).then((r) => {
+        if (r.ok) {
+          r.json().then((user) => onLogin(user));
+        } else {
+          r.json().then((err) => setErrors(err.errors));
+        }
+    })
+  }
   
     return (
       <form onSubmit={handleSubmit}>
@@ -30,6 +38,9 @@ function LoginForm({ onLogin }) {
             onChange={(e) => setPassword(e.target.value)}
         />
         <button type="submit">Login</button>
+        {errors.map((err) => (
+        <h2 key={err}>{err}</h2>
+        ))}
       </form>
     );
 }
