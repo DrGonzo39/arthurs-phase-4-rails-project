996ebb25aa67328296d404004d88bc410b6cc20a
Arthur Benware <artpolitic91@gmail.com>
2023-08-10 12:52:42 -0400

    created album and review models and controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :reviews
+    has_many :albums, through :reviews
     validates :username, :password, :password_confirmation, presence: true 
 end
