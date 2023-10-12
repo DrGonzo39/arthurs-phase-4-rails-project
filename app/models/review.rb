3fcd14266690f2030aab2a23bc2bfdc8e4f23d81
Arthur Benware <artpolitic91@gmail.com>
2023-10-12 17:15:42 -0400

    one last thing

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,13 +3,6 @@ class Review < ApplicationRecord
     belongs_to :album 
     validates :content, length: { maximum: 150 }
 
-    validate :one_per_album
-
     private
 
-    def one_per_album
-        if user.reviews.any? { |review| review.album_id == album.id }
-            errors.add(:base, "You can only review an album once")
-        end 
-    end
 end
